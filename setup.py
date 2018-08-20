fa6351d460696cbfca46341d3cc19704b1b22d7f
Benjamin Maier <benjaminfrankmaier@gmail.com>
2018-08-20 11:05:14 +0200

    changed description

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='cividis',
       version='0.0.1',
-      description="Register the color map `cividis` by Nunez et al. in matplotlib.",
+      description="Register `cividis` with matplotlib, a color map optimized for color vision deficiency, as published in Nuñez, et al. (2018), PLoS ONE 13(7): e0199239 .",
       url='https://www.github.com/benmaier/bfmplot',
       author='Benjamin F. Maier',
       author_email='bfmaier@physik.hu-berlin.de',
